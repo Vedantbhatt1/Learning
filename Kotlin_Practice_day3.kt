e9ed6a985a34d65342f297c0813aa74ea5c41b6e
Vedant Bhatt <115866890+Vedantbhatt1@users.noreply.github.com>
2024-08-21 13:56:42 +0530

    Create Kotlin_Practice_day3.kt

---
+++ Kotlin_Practice_day3.kt
@@ -0,0 +1,52 @@
+/*
+fun main(){
+	val numbers = arrayOf(1,2,3,4,5)
+	val words:Array<String> = arrayOf("aa","bb")
+	val squares = Array(5){i->i*i}
+    var num = arrayOf(1,2,3,4,5)
+    println(numbers[0])
+    println(words[0])
+    println(squares[4])
+    num[0]=9
+    println(num[0])
+    
+    for (num in num){
+        println(num)
+    }
+    
+    
+
+}
+
+*/
+
+// 2D Array
+/*
+fun main(){
+    var twoDarray = arrayOf(arrayOf(1,2,3),arrayOf(4,5,6),arrayOf(7,8,9))
+    println(twoDarray[0][1])
+}
+
+*/
+
+
+// Type Casting
+/*
+fun main(){
+    val x:Int = 100
+    val y:Long = x.toLong()
+    println(y)
+}
+*/
+
+
+// Unsafe and Safe Casting
+/*
+fun main(){
+    var a = "Hello World"
+    var b = a as String
+    println("Unsafe Casting done in $b")
+    var c:Int = a as? Int
+    println("Safe Casting done in $c")
+}
+*/
