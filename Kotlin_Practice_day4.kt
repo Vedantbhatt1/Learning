d1ee8716afcc6aaae23c395ea572f78281170d20
Vedant Bhatt <115866890+Vedantbhatt1@users.noreply.github.com>
2024-08-22 13:53:03 +0530

    Create Kotlin_Practice_day4.kt

---
+++ Kotlin_Practice_day4.kt
@@ -0,0 +1,77 @@
+//Airthmetic operations
+/*
+fun main(){
+    var a = 10
+    var b = 5
+    val isTrue = true
+    val isFalse = false
+    val add = a+b
+    val mul = a*b
+    val sub = a-b
+    val division = a/b
+    val mod = a%b
+    println("Addition of $a and $b is $add")
+    println("Multiplication of $a and $b is $mul")
+    println("Subtraction of $a and $b is $sub")
+    println("Division of $a and $b is $division")
+    println("Modulus of $a and $b is $mod")
+}
+*/
+
+
+//Comparision Operators
+/*
+fun main(){
+    var a = 10
+    var b = 5
+    val isEqual = a==b
+    println("IsEqual : $isEqual")
+}
+*/
+
+
+//Releational Operators
+/*
+fun main(){
+    var a = 11
+    var b =24
+    println("$a is greater than $b : ${a>b}")
+    println("$a is less than $b : ${a<b}")
+    println("$a is greater than or equal to  $b : ${a>=b}")
+    println("$a is less than or equal to $b : ${a<=b}")
+    println("$a is  equal to $b : ${a==b}")
+    println("$a is not equal to $b : ${a!=b}")
+}
+*/
+
+//Assignment Operators
+/*
+fun main(){
+    var a=10
+    val b=3
+    a+=b
+    println(a)
+    a-=b
+    println(a)
+    a*=b
+    println(a)
+    a/=b
+    println(a)
+    a%=b
+    println(a)
+}
+*/
+
+
+//Unary Operators
+/*
+fun main(){
+    var a =24
+    var b = false
+    println("value of +a is ${+a}")
+    println("value of -a is ${-a}")
+    println("value of ++a is ${++a}")
+    println("value of --a is ${--a}")
+    println("value of !b is ${!b}")
+}
+*/
