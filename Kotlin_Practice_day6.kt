581a06c971d36a61bba69b485b843140e6d02617
Vedant Bhatt <115866890+Vedantbhatt1@users.noreply.github.com>
2024-08-27 13:52:48 +0530

    Create Kotlin_Practice_day6.kt

---
+++ Kotlin_Practice_day6.kt
@@ -0,0 +1,74 @@
+// for Loop
+/*
+fun main(){
+    for(item in 1..100){
+        println(item)
+	}
+    for(item in 1 until 100){
+        println(item)
+    }
+}
+*/
+
+// Reverse for Loop
+/*
+fun main(){
+    for(item in 5 downTo -10){
+        println(item)
+    }
+    for(item in 5 downTo -10 step 2){
+        println(item)
+    }
+}
+*/
+
+//Iterating over a List
+/*
+fun main(){
+    val fruits = listOf("Apple","Banana","Cherry")
+    for(fruit in fruits){
+        println(fruit)
+    }
+    for(index in fruits.indices){
+        println("Index:$index --> Fruit:${fruits[index]}")
+    }
+}
+*/
+
+// While Loop
+/*
+fun main(){
+    var i =5
+    while(i>0){
+        println("value is $i")
+        i--
+    }
+}
+*/
+
+// Do-While Loop
+/*
+fun main(){
+    var i=0
+    do{
+        println(i)
+        i++
+    }while(i<50)
+}
+*/
+
+/*
+fun main(){
+    var i =5
+    do{
+        println(i)
+        i--
+    }while(i>0)
+}
+*/
+
+
+fun main(){
+    var i =1
+    do{println(i);i++}while(i<6)
+}
